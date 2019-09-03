d61e0f963dd88086117f3455bd158e0eb734a49e
Leonid Ivanov <Leonid_Ivanov@epam.com>
2019-09-03 11:34:28 +0300

    Initial commit

---
+++ src/main/java/com/epam/java_basic/Application.java
@@ -0,0 +1,15 @@
+package com.epam.java_basic;
+
+import com.epam.java_basic.array_processor.ArrayProcessor;
+
+/**
+ * Application entry point
+ */
+public class Application {
+
+    public static void main(String[] args) {
+        ArrayProcessor processor = new ArrayProcessor();
+        //demonstrate ArrayProcessor methods
+    }
+
+}
