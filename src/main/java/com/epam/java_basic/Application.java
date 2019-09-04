a38f03cbe925b9ced771bd5ad54a997f9c6e2647
Leonid Ivanov <Leonid_Ivanov@epam.com>
2019-09-04 13:52:17 +0300

    Bugs added.

--- src/main/java/com/epam/java_basic/Application.java
+++ src/main/java/com/epam/java_basic/Application.java
@@ -11,7 +11,12 @@ public class Application {
 
     public static void main(String[] args) {
 	int a = 123;
-        ArrayProcessor processor = new ArrayProcessor();
+        try {
+            throw new RuntimeException();
+        } catch (RuntimeException ex) {
+            ArrayProcessor processor = new ArrayProcessor();
+            System.exit(0);
+        }
         //demonstrate ArrayProcessor methods
     }
 
