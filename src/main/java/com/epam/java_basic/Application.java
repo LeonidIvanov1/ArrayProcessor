bc1658a3e9e9f345a8cebf0c6b3a542c2d8a20dc
Leonid Ivanov <Leonid_Ivanov@epam.com>
2019-09-04 14:20:12 +0300

    Bugs added

--- src/main/java/com/epam/java_basic/Application.java
+++ src/main/java/com/epam/java_basic/Application.java
@@ -2,15 +2,24 @@ package com.epam.java_basic;
 
 import com.epam.java_basic.array_processor.ArrayProcessor;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.Formatter;
+
+import static com.sun.xml.internal.ws.spi.db.BindingContextFactory.LOGGER;
+
 /**
  * Application entry point
  */
 public class Application {
 
     public final int maxVAL = 0;
+    private volatile int gunsShootingCounter = 0;
 
     public static void main(String[] args) {
-	int a = 123;
+        Application application = new Application();
+        application.incrementVolatile();
+        int a = 123;
         try {
             throw new RuntimeException();
         } catch (RuntimeException ex) {
@@ -18,6 +27,20 @@ public class Application {
             System.exit(0);
         }
         //demonstrate ArrayProcessor methods
+    }
+
+    private void incrementVolatile() {
+        gunsShootingCounter++;
+        try {
+            FileInputStream fileIn = new FileInputStream("file.txt");
+            FileOutputStream fileOut = new FileOutputStream("copied_file.txt");
+            int a;
+            while ((a = fileIn.read()) != -1) {
+            }
+        } catch (Exception ex) {
+
+        }
+
     }
 
 }
